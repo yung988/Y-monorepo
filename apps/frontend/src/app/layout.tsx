bea8b8611aeea44cb738fb64da069b3744fcef8d
Jan Gajdos <jangajdos@icloud.com>
2025-08-13 07:56:34 +0200

    oprava

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import type React from "react";
 import "./globals.css";
 
 import Footer from "@/components/Footer";
